d6fc144284d5cb08f4b54699bc710620f3dd95e1
MrXiaoM <coolxiaom95@gmail.com>
2023-07-15 13:59:35 +0800

    add security sign impl

--- src/main/kotlin/top/mrxiaom/qsign/QSignService.kt
+++ src/main/kotlin/top/mrxiaom/qsign/QSignService.kt
@@ -1,10 +1,12 @@
 package top.mrxiaom.qsign
 
+import com.tencent.mobileqq.channel.SsoPacket
 import kotlinx.coroutines.*
 import moe.fuqiuluo.api.UnidbgFetchQSign
 import net.mamoe.mirai.internal.spi.EncryptService
 import net.mamoe.mirai.internal.spi.EncryptServiceContext
 import net.mamoe.mirai.internal.spi.EncryptServiceContext.Companion.KEY_BOT_PROTOCOL
+import net.mamoe.mirai.internal.spi.EncryptServiceContext.Companion.KEY_CHANNEL_PROXY
 import net.mamoe.mirai.internal.spi.EncryptServiceContext.Companion.KEY_COMMAND_STR
 import net.mamoe.mirai.internal.spi.EncryptServiceContext.Companion.KEY_DEVICE_INFO
 import net.mamoe.mirai.internal.spi.EncryptServiceContext.Companion.KEY_QIMEI36
@@ -21,10 +23,18 @@ class QSignService(
                 logger.warning("with ${context[CoroutineName]}", e)
             }
         }
-    
+
+    private var channel0: EncryptService.ChannelProxy? = null
+    private val channel: EncryptService.ChannelProxy get() = channel0 ?: throw IllegalStateException("need initialize")
+    private val token = java.util.concurrent.atomic.AtomicBoolean(false)
     @OptIn(MiraiInternalApi::class)
     override fun initialize(context: EncryptServiceContext) {
         val device = context.extraArgs[KEY_DEVICE_INFO]
+
+        channel0 = context.extraArgs[KEY_CHANNEL_PROXY]
+
+        logger.info("register ${context.id}")
+
         UnidbgFetchQSign.register(
             context.id,
             device.androidId.decodeToString(),
@@ -33,12 +43,20 @@ class QSignService(
         )
     }
 
-    override fun encryptTlv(context: EncryptServiceContext, tlvType: Int, payload: ByteArray): ByteArray? {
+    override fun encryptTlv(
+        context: EncryptServiceContext,
+        tlvType: Int,
+        payload: ByteArray
+    ): ByteArray? {
         if (tlvType != 0x544) return null
+        val command = context.extraArgs[KEY_COMMAND_STR]
+
+        logger.info("energy $command")
+
         return runBlocking {
             UnidbgFetchQSign.customEnergy(
                 context.id,
-                context.extraArgs[KEY_COMMAND_STR],
+                command,
                 payload
             )
         }
@@ -51,12 +69,56 @@ class QSignService(
         payload: ByteArray
     ): EncryptService.SignResult? {
         if (commandName == "StatSvc.register") {
-            TODO()
+            if (!token.get() && token.compareAndSet(false, true)) {
+                launch(CoroutineName("RequestToken")) {
+                    while (isActive) {
+                        delay(2400000L)
+                        val request = try {
+                            UnidbgFetchQSign.requestToken(context.id)
+                        } catch (cause: Throwable) {
+                            logger.error(cause)
+                            continue
+                        }
+                        if (request.first) callback(uin = context.id, request = request.second)
+                    }
+                }
+            }
         }
         if (commandName !in cmdWhiteList) return null
-        TODO("Not yet implemented")
+
+        logger.verbose("sign $commandName")
+        val data = runBlocking {
+            UnidbgFetchQSign.sign(uin = context.id, cmd = commandName, seq = sequenceId, buffer = payload)
+        }
+
+        callback(uin = context.id, request = data.requestCallback)
+
+        return EncryptService.SignResult(
+            sign = data.sign,
+            token = data.token,
+            extra = data.extra
+        )
     }
 
+    private fun callback(uin: Long, request: List<SsoPacket>) {
+        launch(CoroutineName("SendMessage")) {
+            for (callback in request) {
+                logger.verbose("Bot(${uin}) sendMessage ${callback.cmd} ")
+                val result = channel.sendMessage(
+                    remark = "mobileqq.msf.security",
+                    commandName = callback.cmd,
+                    uin = 0,
+                    data = callback.body.hexToBytes()
+                )
+                if (result == null) {
+                    logger.debug("${callback.cmd} ChannelResult is null")
+                    continue
+                }
+
+                UnidbgFetchQSign.submit(uin = uin, cmd = result.cmd, callbackId = callback.callbackId, buffer = result.data)
+            }
+        }
+    }
 
     companion object {
         private val logger = MiraiLogger.Factory.create(QSignService::class)
@@ -71,6 +133,7 @@ class QSignService(
             )
             fun register() {
                 Services.register(EncryptService.Factory::class.qualifiedName!!, Factory::class.qualifiedName!!, ::Factory)
+                logger.info("已注册加密算法提供服务")
             }
         }
         override fun createForBot(context: EncryptServiceContext, serviceSubScope: CoroutineScope): EncryptService {
