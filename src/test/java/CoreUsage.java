fb5106d721581e3de787c30173284d778f4dc4a3
MrXiaoM <coolxiaom95@gmail.com>
2023-08-18 15:36:02 +0800

    fix typo

--- src/test/java/CoreUsage.java
+++ src/test/java/CoreUsage.java
@@ -14,7 +14,7 @@ public class CoreUsage {
         // 初始化签名服务，参数为签名服务工作目录，里面应当包含
         // config.json, dtconfig.json, libfekit.so, libQSec.so
         QSignService.Factory.init(new File("txlib/8.9.63"));
-        // 加载签名服务所需协议信息，如果你的协议信息存在非 basePath 的文件夹，请将参数 null 改为协议信息所在目录
+        // 加载签名服务所需协议信息，如果你的协议信息存在非 以上的工作目录 中的文件夹，请将参数 null 改为协议信息所在目录
         // 该方法将会扫描目录下以协议信息命名的文件进行加载，如 android_phone.json
         // 如果你想单独加载协议信息文件，详见 loadProtocolExample() 中的例子
         QSignService.Factory.loadProtocols(null);
